1f540b6ede3e49550595e88aa90db5bc2261d62a
SantiagoIglesia <santiagodelaiglesia29@gmail.com>
2024-02-20 15:48:39 -0500

    Adición transferencia de datos
    Se añadió para transferir datos de vista en vista por js a la vista de editar producto.

--- js/js_products/view_product_js.js
+++ js/js_products/view_product_js.js
@@ -1,13 +1,46 @@
+function capturarDatos(id, nombre_min, categoria){
+  var nombre = nombre_min;
+  var descripcion = document.getElementById('descripcion').innerText;
+  var precio1 = document.getElementById('precio').innerText;
+  var stock = document.getElementById('stock').innerText;
+  var imagen = document.getElementById('imagen').innerText;
+  var destino = document.getElementById('destino').innerText;
+  var proveedor = document.getElementById('proveedor').innerText;
+  var precio2 = 0;
+  localStorage.setItem('id', id);
+  localStorage.setItem('nombre', nombre);
+  localStorage.setItem('descripcion', descripcion);
+  localStorage.setItem('precio1', precio1);
+  localStorage.setItem('stock', stock);
+  localStorage.setItem('imagen', imagen);
+  localStorage.setItem('categoria', categoria);
+  localStorage.setItem('destino', destino);
+  localStorage.setItem('proveedor', proveedor);
+  localStorage.setItem('precio2', precio2);
+}
+
+function detectarCategoria(){
+  var cat = document.getElementById('categoria').innerText;
+  if (cat == "Fritos"){
+    categoria = 1;
+  } else if (cat == "Jugos"){
+    categoria = 2;
+  } else {
+    categoria = 3;
+  }
+  return categoria;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   var url_parametro = new URLSearchParams(window.location.search);
   var id = url_parametro.get('id');
   const imagen = localStorage.getItem('imagen');
-  var nombre = localStorage.getItem('nombre');
+  var nombre_min = localStorage.getItem('nombre');
   var descripcion = localStorage.getItem('descripcion');
   var precio = localStorage.getItem('precio');
   const separador_precio = precio.split(" ");
   var ruta_imagen = imagen.replace(/\.\.\//g, "");
-  nombre = nombre.toUpperCase();
+  nombre = nombre_min.toUpperCase();
   localStorage.removeItem('imagen');
   localStorage.removeItem('nombre');
   localStorage.removeItem('descripcion');
@@ -38,12 +71,25 @@ document.addEventListener("DOMContentLoaded", function() {
     editar_src.addEventListener('click', function(event){
       event.preventDefault();
       alert('Redirigiendo a editar el producto ' + nombre + " con el ID de producto " + id + ".\nEspere un momento...");
+      categoria = detectarCategoria();
+      console.log(categoria);
+      capturarDatos(id, nombre_min, categoria);
       window.location.href = editar_src.src;
     });
   } else if (id == 2) {
     document.getElementById('stock').innerHTML = 12;
-    document.getElementById('categoria').innerHTML = "Fritos";
+    //document.getElementById('categoria').innerHTML = "Fritos";
+    document.getElementById('categoria').innerHTML = "Jugos";
     document.getElementById('destino').innerHTML = "Venta";
     document.getElementById('proveedor').innerHTML = "Sin proveedor";
+    editar_src.src = views + "products" + edit_form + "product" + html_id + id;
+    editar_src.addEventListener('click', function(event){
+      event.preventDefault();
+      alert('Redirigiendo a editar el producto ' + nombre + " con el ID de producto " + id + ".\nEspere un momento...");
+      categoria = detectarCategoria();
+      console.log(categoria);
+      capturarDatos(id, nombre_min, categoria);
+      window.location.href = editar_src.src;
+    });
   }
 });
